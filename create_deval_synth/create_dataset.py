c197a07acb1724839bb187a4569b303be2461449
michellekappl <michelle.kappl@gmail.com>
2025-06-17 12:40:53 +0200

    add more annotation

--- create_deval_synth/create_dataset.py
+++ create_deval_synth/create_dataset.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import re
 
-dataset = pd.read_csv('dataset.txt', names = ["sentence", "hierarchy", "group_x", "group_y"], sep = ";")
+dataset = pd.read_csv('dataset_old.txt', names = ["sentence", "hierarchy", "group_x", "group_y"], sep = ";")
 nan_val = dataset[dataset['group_y'].isna()]
 
 if not nan_val["group_x"].isna().all():
     dataset.loc[nan_val.index, "group_x"] = "[]"
     dataset.loc[nan_val.index, "group_y"] = "[]"
 
-dataset.loc[dataset['hierarchy'].str.contains(r'\[|\]', na=False), 'hierarchy'] = None
-dataset.loc[dataset['group_x'].str.contains(r'\[none\]', na=False), 'group_x'] = None
+dataset.loc[dataset['hierarchy'].str.contains(r'\[|\]', na=False), 'hierarchy'] = "none"
+dataset.loc[dataset['group_x'].str.contains(r'\[none\]', na=False), 'group_x'] = "none"
 #dataset.loc[dataset['hierarchy'].str.contains(r'unklar', na=False), 'hierarchy'] = None
 #dataset.loc[dataset['hierarchy'].str.contains(r'x=y', na=False), 'hierarchy'] = None
+dataset.loc[dataset['group_x'].str.contains(r'^(?!.*x_).+$', na=False), 'group_x'] = "[]"
+dataset.loc[dataset['group_y'].str.contains(r'^(?!.*y_).+$', na=False), 'group_y'] = "[]"
 
 dataset = dataset.replace({pd.NA: None})
 
